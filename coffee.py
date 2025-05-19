b8a1220c11c103e4304bd87bab0bc960454a3048
ReaganBaimet0 <reagan.baimet@student.moringaschool.com>
2025-05-19 18:48:13 +0300

    new

--- coffee.py
+++ coffee.py
@@ -1,12 +1,29 @@
-class coffee:
-    def __init__(self,name):
-     if not isinstance(name, str):
-        raise TypeError("Name must be a string.")
-     if len(name) < 3:
-        raise ValueError("Name must be at least 3 characters long.")
-     self._name = name
+# coffee.py
 
-     @property
-     def name(self):
-            return self._name
+class Coffee:
+    def __init__(self, name):
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string.")
+        if len(name) < 3:
+            raise ValueError("Name must be at least 3 characters long.")
+        self._name = name
+        self._orders = []
 
+    @property
+    def name(self):
+        return self._name
+
+    def add_order(self, order):
+        """Add an Order instance to this coffee."""
+        self._orders.append(order)
+
+    def orders(self):
+        """Return all Order instances for this coffee."""
+        return self._orders
+
+    def customers(self):
+        """Return a unique list of Customer instances who ordered this coffee."""
+        return list({order.customer for order in self._orders})
+
+    def __repr__(self):
+        return f"Coffee('{self.name}')"
--- customer.py
+++ customer.py
@@ -1,6 +1,9 @@
+# customer.py
+
 class Customer:
     def __init__(self, name):
-        self.name = name 
+        self.name = name  # Triggers the @name.setter validation
+        self._orders = []
 
     @property
     def name(self):
@@ -13,3 +16,18 @@ class Customer:
         if not (1 <= len(value) <= 15):
             raise ValueError("Name must be between 1 and 15 characters.")
         self._name = value
+
+    def add_order(self, order):
+        """Add an Order instance to this customer."""
+        self._orders.append(order)
+
+    def orders(self):
+        """Return all Order instances for this customer."""
+        return self._orders
+
+    def coffees(self):
+        """Return a unique list of Coffee instances the customer has ordered."""
+        return list({order.coffee for order in self._orders})
+
+    def __repr__(self):
+        return f"Customer('{self.name}')"
